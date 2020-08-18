a8d1843da16f628d832099148dc3070230dcb625
DuongMai <mtvd1001@gmail.com>
2020-08-18 20:17:51 +0700

    Init project

---
+++ demoApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoApplication
+//
+//  Created by Macbook  on 8/18/20.
+//  Copyright © 2020 mtvd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
